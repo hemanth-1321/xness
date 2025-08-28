3c2af39fe92f2ef62732ee019cff7408f0a36167
hemanth-2113 <hemanth02135@gmail.com>
2025-08-28 19:47:40 +0530

    subscribe_to_trades

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,8 +1,20 @@
 import client from "@repo/db/client"
 import express from "express"
+import cors from "cors"
+import{createServer} from 'http'
+import {Server} from 'socket.io'
+import{subscriber}from '@repo/redis/redis-client'
 
 const app = express()
+app.use(cors())
 const PORT = 8080
+
+const server =createServer(app)
+const io=new Server(server,{
+  cors:{
+    origin:"*"
+  }
+})
 
 const timeFramemap: Record<string, string> = {
   "1m": "candles_1m",
@@ -14,6 +26,10 @@ const timeFramemap: Record<string, string> = {
 }
 
 app.get("/health", (_, res) => res.send("hello world"))
+
+/**
+ * GET end point to get candles based on time frame
+ */
 
 app.get("/candles/:timeframe", async (req, res) => {
   const { timeframe } = req.params
@@ -28,18 +44,76 @@ app.get("/candles/:timeframe", async (req, res) => {
       `SELECT * FROM ${viewName} WHERE ($1::text IS NULL OR symbol = $1) ORDER BY bucket DESC LIMIT $2;`,
       [symbol || null, limitInt]
     )
-    res.json(result.rows)
+   res.json(result.rows.reverse())
   } catch (err) {
     console.error("Error fetching candles:", err)
     res.status(500).json({ error: "Database query failed" })
   }
 })
 
+
+/**
+ * socket io and subscribe
+ */
+
+io.on("connection",(socket)=>{
+console.log("client connected",socket.id);
+
+  socket.on("subscribe-trades", async (data:{symbol:string}) => {
+    const {symbol}=data
+    const room = `trades-${symbol}`;
+    await socket.join(room);
+    console.log(`Client subscribed to room: ${room}`);
+  });
+
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+
+
+})
+
+
+async function setRedis() {
+  if (subscriber.status !== "ready") {
+    await subscriber.connect();
+    console.log("Redis subscriber connected");
+  }
+
+  await subscriber.subscribe("trade-channel"); // just subscribe
+
+  subscriber.on("message", (channel, message) => {
+    console.log("Raw Redis message:", message);
+
+    if (!message || typeof message !== "string") {
+      console.error("Received non-string message:", message);
+      return;
+    }
+
+    try {
+      const data = JSON.parse(message);
+      const symbol = data.trade?.data?.s;
+      if (symbol) {
+        const room = `trades-${symbol}`;
+        io.to(room).emit("live-trade", data.trade);
+        console.log(`Emitted trade to ${room}:`, data.trade);
+      }
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  });
+}
+
+
+
+
 ;(async () => {
   try {
     await client.connect()
     console.log("Connected to DB")
-    app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
+    server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
+    await setRedis()
   } catch (err) {
     console.error("DB connection failed:", err)
     process.exit(1)

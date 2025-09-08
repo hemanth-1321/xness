3c045139cb16a0fcd75074651b592f9f76b29631
hemanth-2113 <hemanth02135@gmail.com>
2025-09-08 19:24:17 +0530

    workflows

--- apps/price-pooler/src/index.ts
+++ apps/price-pooler/src/index.ts
@@ -1,41 +1,33 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import WebSocket from "ws";
-import { publisher } from "@repo/redis/redis-client"
+import { publisher } from "@repo/redis/redis-client";
 import { Trade } from "./types";
 
+const binanceWS = process.env.BINANCE_API_KEY!;
 
-const binanceWS = process.env.BINANCE_API_KEY!
-
-const ws = new WebSocket(binanceWS)
+const ws = new WebSocket(binanceWS);
 
 ws.on("open", () => {
-  console.log("connected to binance websocket")
-})
-
-
+  console.log("connected to binance websocket");
+});
 
 ws.on("message", async (msg) => {
   try {
     // console.log("raw msg", JSON.parse(msg.toString()));
-    const data: Trade = JSON.parse(msg.toString())
-
+    const data: Trade = JSON.parse(msg.toString());
 
     if (data.stream && data.data) {
-      //publishes data 
-      await publisher.publish("trade-channel", JSON.stringify(data))
-      console.log('data published',data)
-    
-      
+      //publishes data
+      await publisher.publish("trade-channel", JSON.stringify(data));
+      console.log("data published", data);
 
       // trades are queued to push into data base (timescale db)
-      await publisher.rpush("trade-queue", JSON.stringify(data))
+      await publisher.rpush("trade-queue", JSON.stringify(data));
 
       // console.log('data queued', data.data.p)
-
     }
   } catch (error) {
-    console.error("error streaming binance api", error)
+    console.error("error streaming binance api", error);
   }
-})
-
+});

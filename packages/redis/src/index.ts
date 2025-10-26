0c1239b24b85fffd16fe5d7fc98015ab7a2e7464
j-jerusha <jerusha.jp2004@gmail.com>
2025-10-26 14:39:45 +0530

    all

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,8 +1,6 @@
-// redis-client.ts
 import Redis from "ioredis";
 
 const REDIS_URL = process.env.REDIS_URL;
-
 export const publisher = new Redis(REDIS_URL!);
 publisher.on("connect", () => console.log("Publisher connected"));
 

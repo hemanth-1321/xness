ae179815e9d121373fe4252307817d44da630c71
j-jerusha <jerusha.jp2004@gmail.com>
2025-10-26 14:48:54 +0530

    dotenv

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,5 +1,6 @@
 import Redis from "ioredis";
-
+import dotenv from "dotenv";
+dotenv.config();
 const REDIS_URL = process.env.REDIS_URL;
 export const publisher = new Redis(REDIS_URL!);
 publisher.on("connect", () => console.log("Publisher connected"));
